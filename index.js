a3d28ba7ae3df7838fcbbe2b19142e14a14e5d34
Fayez-fyz <zeyaffayez007@gmail.com>
2021-10-25 15:47:12 +0530

    Intial-commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { readdirSync } from "fs";
+
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+
+//db
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB IS CONNECTED"))
+  .catch((err) => console.log("DB CONNECTION ERROR", err));
+
+//middlewares
+app.use(express.json({ limit: "6mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
---
+++ middlewares/index.js
@@ -0,0 +1,20 @@
+import expressJwt from "express-jwt";
+import Post from "../models/post";
+export const requiresSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+});
+
+export const canEditDeletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params._id);
+    console.log("POST in edit delete Middleware", post);
+    if (req.user._id != post.postedBy) {
+      return res.status(400).send("Unauthorized");
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
